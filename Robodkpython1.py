b01d2ac501f71e66dd3005145e2ac6ea627f07ce
SistemasEmbebidos <78398897+SistemasEmbebidos2020@users.noreply.github.com>
2022-07-04 10:22:40 -0500

    Update Robodkpython1.py

--- Robodkpython1.py
+++ Robodkpython1.py
@@ -14,19 +14,21 @@ posi = Pose_2_TxyzRxyz(pose_ref)
 print (posi)
 home_joints =  robot.JointsHome()
 print (home_joints)
-robot.MoveJ(home_joints)
+#robot.MoveJ(home_joints)
 pose_ref=robot.Pose()
 
 limitneg,limitpos,pp = robot.JointLimits()
 print (limitneg,  "\n" ,limitpos ,"\n" )
 aprox = 100
-a0 = transl(850,0,400+aprox)
-a1 = transl(850,0,400)
-a2 = transl(750,0,400)
-a3 = transl(750,50,400)
-a4 = transl(850,50,400)
+a0 = transl(-370,50,-50+aprox) 
+a1 = transl(-370,50,-150)
+a2 = transl(-370,50,50)
+a3 = transl(-370,-150,50)
+a4 = transl(-370,-150,-50)
+a5 = transl(-370,50,-50)
+a6 = transl(-370,-150,-150)
 
-A = [a0, a1, a2, a3, a4]
+A = [a0, a1, a2, a3, a4,a5,a6]
 
-for i in range(5):
+for i in range(len(A)):
     robot.MoveJ(A[i]*rotx(-pi))
